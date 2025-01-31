123d299fffde055dfe1f891e25dd0d94499b7da5
Bryan Chau <bryanchau@Bryans-MacBook-Pro.local>
2025-01-31 19:25:40 +0700

    changed back to dynamic ipaddress

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ import {NetworkInfo} from 'react-native-network-info';
 
 const config = async () => {
   try {
-    // const ipAddress = await NetworkInfo.getIPAddress();
-    const ipAddress = '172.20.10.2';
+    const ipAddress = await NetworkInfo.getIPAddress();
+    // console.log(ipAddress, 'ipAddress in config');
+    // const ipAddress = '172.20.10.2'; //phone ipaddress
     return {
       apiUrl: `http://${ipAddress}:3000`,
     };
