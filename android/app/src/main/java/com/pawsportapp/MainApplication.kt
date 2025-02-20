ad538c57e859e375e1923a9d601fd24ff4822fea
Bryan Chau <bryanchau@Bryans-MacBook-Pro.local>
2025-02-20 19:53:47 +0700

    adjusted all naming to be Pawsport

--- android/app/src/main/java/com/pawsportapp/MainApplication.kt
+++ android/app/src/main/java/com/pawsportapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.travelcollabapp
+package com.pawsportapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/Pawsport/AppDelegate.mm
+++ ios/Pawsport/AppDelegate.mm
@@ -8,7 +8,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [GMSServices provideAPIKey:@"AIzaSyAEEHT0dHSKuklxdI-L7q8byjZnG24-Quw"]; // add this line using the api key obtained from Google Console
-  self.moduleName = @"travelCollabApp";
+  self.moduleName = @"Pawsport";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
