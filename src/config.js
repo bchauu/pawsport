3aed9cd2dffac6b5a1182c6af94e66c058454c7c
Bryan Chau <bryanchau@Bryans-MacBook-Pro.local>
2025-02-23 16:24:35 +0700

    added domain for apiurl and commented out local

--- src/config.js
+++ src/config.js
@@ -6,7 +6,9 @@ const config = async () => {
     // console.log(ipAddress, 'ipAddress in config');
     // const ipAddress = '172.20.10.2'; //phone ipaddress
     return {
-      apiUrl: `http://${ipAddress}:3000`,
+      // apiUrl: `http://${ipAddress}:3000`,
+      // apiUrl: 'http://3.106.1.235:3000', // Replace with your Elastic IP
+      apiUrl: 'https://api.pawsport.quest', // Replace with your domain
     };
   } catch (error) {
     console.error('Error getting IP address:', error);
